b18c43e300c0b52af5b8ba071e102502df7dddf5
paldooy <kemoywashere@gmail.com>
2026-04-19 23:54:58 +0800

    Penyesuaian DB

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./ecommerce.db"
+# Ganti 'ecommerce_db' dengan nama database yang Anda buat di Laragon
+# Format: mysql+pymysql://username:password@host:port/nama_database
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/ecommerce_db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Hapus connect_args={"check_same_thread": False} karena itu khusus untuk SQLite
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
